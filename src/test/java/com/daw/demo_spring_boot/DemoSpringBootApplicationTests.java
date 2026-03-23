0a00c710babe296691baad25ea9eb86fb5c26cf1
BryanStrk <bryan_ed_8@hotmail.com>
2026-03-23 15:25:44 +0100

    First commitment of the Spring Boot project

---
+++ src/test/java/com/daw/demo_spring_boot/DemoSpringBootApplicationTests.java
@@ -0,0 +1,18 @@
+package com.daw.demo_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootApplicationTests {
+
+
+
+    @Test
+    void contextLoads() {
+
+    }
+
+
+
+}
