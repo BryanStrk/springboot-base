69d6d3dd564fd686444b9bc3c7d7fb35257d67c8
BryanStrk <bryan_ed_8@hotmail.com>
2026-03-24 08:53:52 +0100

    cambios en el properties

--- src/main/java/com/daw/demo_spring_boot/DemoSpringBootApplication.java
+++ src/main/java/com/daw/demo_spring_boot/DemoSpringBootApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoSpringBootApplication {
 
-    public static void main(String[] args) {
-//        SpringApplication.run(DemoSpringBootApplication.class, args);
-        System.out.println("Hello World!");
+     static void main(String[] args) {
+        SpringApplication.run(DemoSpringBootApplication.class, args);
+
     }
 
 }
